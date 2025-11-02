be360ba6f5a58f04d6969a8366dda9ae00fe495f
Fernando del olmo <fernando@openforge.io>
2025-11-02 12:15:00 +0100

    feat(core): add android platform

---
+++ android/app/src/main/java/com/fdom/pantry/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fdom.pantry;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'pantry-manager',
+  appId: 'com.fdom.pantry',
+  appName: 'Mi Despensa',
   webDir: 'www'
 };
 
