36a696a0e693939cd63cecfea3b17913ad0a253a
Fernando del olmo <fernando@openforge.io>
2025-11-27 10:31:07 +0100

    feat(core): configured envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
+  production: true,
+  apiBaseUrl: 'https://pantry-manager.onrender.com',
+  revenueCatPublicKey: 'goog_QWDXnEcbnxIKMCjuMLHcOtwxmrR',
 };
