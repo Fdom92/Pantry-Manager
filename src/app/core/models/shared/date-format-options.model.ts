407f488cab100b786cc0d9251b8ce73a91423c0d
Fernando del olmo <fernando@openforge.io>
2026-03-04 07:37:27 +0100

    fix(): standard date format

--- src/app/core/models/shared/date-format-options.model.ts
+++ src/app/core/models/shared/date-format-options.model.ts
@@ -1,11 +1,17 @@
 export interface LocaleDateFormatOptions {
   readonly numeric: Intl.DateTimeFormatOptions;
+  readonly short: Intl.DateTimeFormatOptions;
 }
 
 export const ES_DATE_FORMAT_OPTIONS: LocaleDateFormatOptions = {
   numeric: {
     day: '2-digit',
     month: '2-digit',
+    year: 'numeric',
+  },
+  short: {
+    day: 'numeric',
+    month: 'short',
     year: 'numeric',
   },
 };
